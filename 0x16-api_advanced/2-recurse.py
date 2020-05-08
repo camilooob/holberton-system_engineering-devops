72e78368ba3879534a9e07c628b679d07def04f5
camilooob <camilobaq@hotmail.com>
2020-05-07 20:33:29 -0500

    try reddi

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Recursive comments"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    '''Return subreddit'''
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+        subreddit, after)
+    header = {'User-Agent': 'your bot 0.1'}
+    response = requests.get(url, headers=header, allow_redirects=False)
+
+    if response:
+        data = response.json().get('data')
+        children = data.get('children')
+        [hot_list.append(x.get('data').get('title')) for x in children]
+        after = data.get('after')
+        if after:
+            return recurse(subreddit, hot_list, after)
+        return hot_list
+    return None
